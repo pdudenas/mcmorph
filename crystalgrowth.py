e038a35958382608a47f840e2e070fe9db5d8b28
David Ackerman <user@none.invalid>
2020-09-08 12:51:50 -0500

    Make nucleation work in 3D

--- crystalgrowth.py
+++ crystalgrowth.py
@@ -1,5 +1,6 @@
 import numpy as np
 from skimage.filters import sobel
+import random
 
 
 def binarize(array):
@@ -7,11 +8,40 @@ def binarize(array):
     binary_array[~np.isnan(array)] = 1
     return binary_array
 
-def nucleate(blank_array, nucleation_sites):
-    positions = np.random.randint(0,len(blank_array),size=(nucleation_sites,2))
-    orientations = np.random.uniform(-np.pi/2,np.pi/2,size=(nucleation_sites))
-    blank_array[positions[:,0],positions[:,1]] = orientations
+
+def nucleate(blank_array, nucleation_sites, random_seed=None):
+    """Seeds the structure with given number of sites
+
+    Sites are selected randomly in the domain.
+
+    At each site, an orientation in [-pi/2, pi/2) is selected and assigned to the given location.
+
+    There is no check for distance between sites or
+    to ensure sites are unique.
+
+    Passing a random seed allows reproducible results. If no seed is passed, the generator will not be seeded
+    and should produce random results each time.
+
+    This function is dimension independent so will work in both 2d and 3d
+
+    Args:
+        blank_array: array to fill with seeds
+        nucleation_sites: number of nucleation sites to use
+        random_seed: seed for random number generator
+
+    Returns:
+        A tuple with (the seeded array, a list of the seed locations, the orientation at each location)
+    """
+    if random_seed is not None:
+        np.random.seed(random_seed)
+
+    positions = [[np.random.randint(0, dim) for dim in blank_array.shape] for i in range(nucleation_sites)]
+    positions = np.array(positions)
+    orientations = np.random.uniform(-np.pi/2, np.pi/2, size=nucleation_sites)
+    for pos, orient in zip(positions, orientations):
+        blank_array[tuple(pos)] = orient
     return blank_array, positions, orientations
+
 
 def fill_nan_localmean(array,nan_idx):
 #     nan_idx = np.column_stack(np.where(np.isnan(array)))
