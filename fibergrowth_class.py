2db75d0ded4c36e31c675cc68109d89b8162b711
pdudenas <64651139+pdudenas@users.noreply.github.com>
2021-02-06 19:32:36 -0500

    Added helical fiber growth

--- fibergrowth_class.py
+++ fibergrowth_class.py
@@ -71,23 +71,7 @@ class fibergrowth():
 
         return theta
 
-    def expand_fiber(self,xlist,ylist,theta,width,fiberspace):
-        ''' assign orientation orthogonal to fiber core out to some width '''
-        fiber_count = np.zeros(fiberspace.shape)
-        fiber_orientation = fiber_count.copy()
-        fiber_center = fiber_count.copy()
-        r = np.linspace(-width,width,4*width)
-
-        for i, (x,y) in enumerate(zip(xlist,ylist)):
-            dx = np.round(x + r*np.cos(np.pi/2+theta[i]))%fiberspace.shape[1]
-            dy = np.round(y + r*np.sin(np.pi/2+theta[i]))%fiberspace.shape[0]
-            fiber_count[dy.astype(int),dx.astype(int)] += 1
-            fiber_orientation[dy.astype(int),dx.astype(int)] += theta[i]
-            centerx = np.round(x)%fiberspace.shape[1]
-            centery = np.round(y)%fiberspace.shape[0]
-            fiber_center[centery.astype(int),centerx.astype(int)] = 1
-
-        fiber_orientation /= fiber_count
+    def fill_nans(self,fiber_center,fiber_count,fiber_orientation,width,theta):
         # fill in missing holes using fiber from disk structuring element
         fiber_center = dilation(fiber_center,disk(width))
         fill_idx = (fiber_center == 1) & (np.isnan(fiber_orientation))
@@ -104,6 +88,34 @@ class fibergrowth():
 
         # go back and make sure fiber_count matches
         fiber_count[fiber_orientation != 0] = 1
+        return fiber_count, fiber_orientation
+
+    def expand_fiber(self,xlist,ylist,theta,width,fiberspace,fiber_count,
+                                                            fiber_orientation,
+                                                            fiber_center):
+        ''' assign orientation orthogonal to fiber core out to some width '''
+
+        # zero out fiber_count, fiber_orientation, and fiber_center
+        fiber_count[:] = 0
+        fiber_orientation[:] = 0
+        fiber_center[:] = 0
+        r = np.linspace(-width,width,4*width)
+
+        for i, (x,y) in enumerate(zip(xlist,ylist)):
+            dx = np.round(x + r*np.cos(np.pi/2+theta[i]))%fiberspace.shape[1]
+            dy = np.round(y + r*np.sin(np.pi/2+theta[i]))%fiberspace.shape[0]
+            fiber_count[dy.astype(int),dx.astype(int)] += 1
+            fiber_orientation[dy.astype(int),dx.astype(int)] += theta[i]
+            centerx = np.round(x)%fiberspace.shape[1]
+            centery = np.round(y)%fiberspace.shape[0]
+            fiber_center[centery.astype(int),centerx.astype(int)] = 1
+
+        fiber_orientation /= fiber_count
+
+        fiber_count, fiber_orientation = self.fill_nans(fiber_center,
+                                                        fiber_count,
+                                                        fiber_orientation,
+                                                        width,theta)
 
         return fiber_count, fiber_orientation
 
@@ -111,6 +123,12 @@ class fibergrowth():
                     fiberspace_size, fiber_length=None):
         fiberspace = np.zeros((fiberspace_size,fiberspace_size))
         alignment_space = fiberspace.copy()
+
+        # pre-allocate here once, instead of every loop in expand_fiber
+        fiber_count = fiberspace.copy()
+        fiber_orientation = fiberspace.copy()
+        fiber_center = fiberspace.copy()
+
 
 
         if fiber_length is None:
@@ -128,10 +146,145 @@ class fibergrowth():
                                                                ylist,
                                                                theta,
                                                                fiber_width,
-                                                               fiberspace)
+                                                               fiberspace,
+                                                               fiber_count,
+                                                               fiber_orientation,
+                                                               fiber_center)
             fiberspace += fiber_count
             alignment_space += fiber_orientation
 
         alignment_space /= fiberspace
 
         return fiberspace, alignment_space
+
+    def tangent_components(self,scale,t,theta):
+        xcomp = 1
+        ycomp = scale*np.cos(t*scale)
+        zcomp = scale*np.sin(t*scale)
+        xout = xcomp*np.cos(theta) - ycomp*np.sin(theta)
+        yout = xcomp*np.sin(theta) + ycomp*np.cos(theta)
+        return xout, yout, zcomp
+
+    def normal_components(self,scale,t,theta):
+        xcomp = 0
+        ycomp = -scale**2*np.sin(t*scale)
+        zcomp = scale**2*np.cos(t*scale)
+        xout = xcomp*np.cos(theta) - ycomp*np.sin(theta)
+        yout = xcomp*np.sin(theta) + ycomp*np.cos(theta)
+        return xout, yout, zcomp
+
+    def fill_nans_helical(self, fiber_center,tan_comps,norm_comps,width,theta,scale):
+        fiber_center=dilation(fiber_center,disk(width))
+
+        fill_idx = (fiber_center == 1) & (np.isnan(np.sum(tan_comps,axis=2)))
+        fill2 = np.where(fill_idx == True)
+        for x,y in zip(fill2[1],fill2[0]):
+            tan_comps[y,x,:] = np.nanmean(tan_comps[(y-1):(y+2),(x-1):(x+2),:],axis=(0,1))
+            norm_comps[y,x,:] = np.nanmean(norm_comps[(y-1):(y+2),(x-1):(x+2),:],axis=(0,1))
+        # get the first bit of the fiber that wasn't caught with nanmean
+        fill_idx = (fiber_center == 1) & (np.isnan(tan_comps[:,:,0]))
+        fill2 = np.where(fill_idx == True)
+        tan_comps[fill2[0].astype(int),fill2[1].astype(int),:] = self.tangent_components(scale,0,theta[0])
+        norm_comps[fill2[0].astype(int),fill2[1].astype(int),:] = self.normal_components(scale,0,theta[0])
+
+        # replace nans with zeros
+        nan_idx = np.isnan(tan_comps)
+        tan_comps[nan_idx] = 0
+        norm_comps[nan_idx] = 0
+        # make sure fiber_center and component arrays are consistent
+        fiber_center[tan_comps[:,:,0] != 0] = 1
+
+        return fiber_center, tan_comps, norm_comps
+
+    def map_helical(self,xlist,ylist,theta,width,scale,
+                                                fiberspace,
+                                                fiber_count,
+                                                tan_comps,
+                                                norm_comps,
+                                                fiber_center):
+        # reset arrays
+        tan_comps[:] = 0
+        norm_comps[:] = 0
+        fiber_count[:] = 0
+        fiber_center[:] = 0
+
+        r = np.linspace(-width,width,4*width)
+
+        for i, (x,y) in enumerate(zip(xlist,ylist)):
+            centerx = np.round(x)%fiberspace.shape[1]
+            centery = np.round(y)%fiberspace.shape[0]
+            fiber_center[centery.astype(int),centerx.astype(int)] = 1
+            
+            dx = np.round(x + r*np.cos(np.pi/2+theta[i]))%fiberspace.shape[1]
+            dy = np.round(y + r*np.sin(np.pi/2+theta[i]))%fiberspace.shape[0]
+            fiber_count[dy.astype(int),dx.astype(int)] += 1
+
+            tan_comps[dy.astype(int),dx.astype(int),:] = self.tangent_components(scale,i,theta[i])
+            norm_comps[dy.astype(int),dx.astype(int),:] = self.normal_components(scale,i,theta[i])
+
+        tan_comps /= fiber_count[:,:,np.newaxis]
+        norm_comps /= fiber_count[:,:,np.newaxis]
+
+        fiber_center, tan_comps, norm_comps = self.fill_nans_helical(fiber_center,
+                                                                    tan_comps,
+                                                                    norm_comps,
+                                                                    width,
+                                                                    theta,
+                                                                    scale)
+        return fiber_center, tan_comps, norm_comps
+
+
+
+    def grow_helicalfibers(self,fiber_number,director,sigma1,sigma2,fiber_width,avg_width,
+                    fiberspace_size, helical_scale, fiber_length=None):
+        # pre-allocate here once, instead of every loop in expand_fiber
+        fiberspace = np.zeros((fiberspace_size,fiberspace_size))
+        fiber_count = fiberspace.copy()
+        fiber_center = fiberspace.copy()
+
+        alignment_tan = np.zeros((fiberspace_size,fiberspace_size,3))
+        alignment_norm = alignment_tan.copy()
+        tan_comps = alignment_tan.copy()
+        norm_comps = alignment_tan.copy()
+
+        if fiber_length is None:
+            fiber_length = fiberspace_size
+
+        for i in range(fiber_number):
+            mu = self.rng.normal(director,sigma1)
+
+            xlist, ylist = self.grow_fiber_core(fiber_length,mu,sigma2,
+                            fiberspace_size)
+
+            xsmooth, ysmooth = self.smooth_fiber_core(xlist,ylist,avg_width)
+            theta = self.axial_director(xsmooth,ysmooth)
+            fiber_count, tan_comps, norm_comps = self.map_helical(xlist,
+                                                               ylist,
+                                                               theta,
+                                                               fiber_width,
+                                                               helical_scale,
+                                                               fiberspace,
+                                                               fiber_count,
+                                                               tan_comps,
+                                                               norm_comps,
+                                                               fiber_center)
+            fiberspace += fiber_count
+            alignment_tan += tan_comps
+            alignment_norm += norm_comps
+
+        # divide sum of tangent and normal componenets by number of fibers
+        alignment_tan /= fiberspace[:,:,np.newaxis]
+        alignment_norm /= fiberspace[:,:,np.newaxis]
+
+        alignment_tan[np.isnan(alignment_tan)] = 0
+        alignment_norm[np.isnan(alignment_norm)] = 0
+
+        # make sure magnitude of tangential and normal components are both 1
+        dmag = np.sqrt(np.sum(alignment_tan**2,axis=2))
+        d2mag = np.sqrt(np.sum(alignment_norm**2,axis=2))
+        alignment_tan /= dmag[:,:,np.newaxis]
+        alignment_norm /= d2mag[:,:,np.newaxis]
+        # calculate binormal vector array from cross product
+        alignment_binorm = np.cross(alignment_tan,alignment_norm)
+
+        return fiberspace, alignment_tan, alignment_norm, alignment_binorm
